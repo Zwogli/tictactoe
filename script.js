deca8660255301f99d856547d84c94c0cbd6538c
Zwogli <Mathiaskohler@web.de>
2023-04-03 11:27:30 +0200

    cleanCode

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
 let fields = [],
     currentShape = 'cross',
     gameOver = false,
-    move = 0;
+    move = 0,
+    winner;
 
 function fillShape(id){
   if (!fields[id] && !gameOver) {        //* ! = false , !! = true, ! = invert boolean 
@@ -34,54 +35,70 @@ function draw(){
 }
 
 function checkForWin(){
-let winner;
+checkForWinHorizontal();
+checkForWinVertical();
+checkForWinDiagonal();
 
-  if (fields[0] == fields[1] && fields[1] == fields[2] && fields[0]) {
-    winner = fields[0];
-    document.getElementById('line-0').style.transform = 'scaleX(1.2)';
+finishGame();
 }
-  if (fields[3] == fields[4] && fields[4] == fields[5] && fields[3]) {
-  winner = fields[3];
-  document.getElementById('line-1').style.transform = 'scaleX(1.2)';
+
+function checkForWinHorizontal(){
+if (fields[0] == fields[1] && fields[1] == fields[2] && fields[0]) {
+  winner = fields[0];
+  document.getElementById('line-0').style.transform = 'scaleX(1.2)';
 }
-  if (fields[6] == fields[7] && fields[7] == fields[8] && fields[6]) {
-  winner = fields[6];
-  document.getElementById('line-2').style.transform = 'scaleX(1.2)';
+if (fields[3] == fields[4] && fields[4] == fields[5] && fields[3]) {
+winner = fields[3];
+document.getElementById('line-1').style.transform = 'scaleX(1.2)';
 }
+if (fields[6] == fields[7] && fields[7] == fields[8] && fields[6]) {
+winner = fields[6];
+document.getElementById('line-2').style.transform = 'scaleX(1.2)';
+}
+}
+
+function checkForWinVertical(){
   if (fields[0] == fields[3] && fields[3] == fields[6] && fields[0]) {
-  winner = fields[0];
-  document.getElementById('line-3').style.transform = 'rotate(90deg) scaleX(1.2)';
+    winner = fields[0];
+    document.getElementById('line-3').style.transform = 'rotate(90deg) scaleX(1.2)';
+  }
+    if (fields[1] == fields[4] && fields[4] == fields[7] && fields[1]) {
+    winner = fields[1];
+    document.getElementById('line-4').style.transform = 'rotate(90deg) scaleX(1.2)';
+  }
+    if (fields[2] == fields[5] && fields[5] == fields[8] && fields[2]) {
+    winner = fields[2];
+    document.getElementById('line-5').style.transform = 'rotate(90deg) scaleX(1.2)';
+  }
 }
-  if (fields[1] == fields[4] && fields[4] == fields[7] && fields[1]) {
-  winner = fields[1];
-  document.getElementById('line-4').style.transform = 'rotate(90deg) scaleX(1.2)';
-}
-  if (fields[2] == fields[5] && fields[5] == fields[8] && fields[2]) {
-  winner = fields[2];
-  document.getElementById('line-5').style.transform = 'rotate(90deg) scaleX(1.2)';
-}
+
+function checkForWinDiagonal(){
   if (fields[0] == fields[4] && fields[4] == fields[8] && fields[0]) {
-  winner = fields[0];
-  document.getElementById('line-6').style.transform = 'rotate(45deg) scaleX(1.2)';
+    winner = fields[0];
+    document.getElementById('line-6').style.transform = 'rotate(45deg) scaleX(1.2)';
+  }
+    if (fields[2] == fields[4] && fields[4] == fields[6] && fields[2]) {
+    winner = fields[2];
+    document.getElementById('line-7').style.transform = 'rotate(-45deg) scaleX(1.2)';
+  }
 }
-  if (fields[2] == fields[4] && fields[4] == fields[6] && fields[2]) {
-  winner = fields[2];
-  document.getElementById('line-7').style.transform = 'rotate(-45deg) scaleX(1.2)';
-}
-if (winner || move == 9) {
-  console.log('Winner', winner);
-  gameOver = true;
-  setTimeout(function(){
-    document.getElementById('game-over').classList.remove('d-none');
-    document.getElementById('restart-btn').classList.remove('d-none');
-  }, 250);
-}
+
+function finishGame(){
+  if (winner || move == 9) {
+    console.log('Winner', winner);
+    gameOver = true;
+    setTimeout(function(){
+      document.getElementById('game-over').classList.remove('d-none');
+      document.getElementById('restart-btn').classList.remove('d-none');
+    }, 250);
+  }
 }
 
 function restart(){
   gameOver = false;
   fields = [];
   move = 0;
+  winner = undefined;
   document.getElementById('game-over').classList.add('d-none');
   document.getElementById('restart-btn').classList.add('d-none');
 
